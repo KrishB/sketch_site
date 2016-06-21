bfe1018b105d37d12bf682870ee81c9f2304fb86
Krish <bkrisanthan@gmail.com>
2016-06-21 19:15:12 -0400

    added working grid

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-	
+	for(var i = 0; i < 16; i++){
+		$("#container").append("<div class='square'></div>");
+	}
+	$(".square").hover(function(){
+		$(this).css("background-color", "blue");
+	});
 });
