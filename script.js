49825889e660a8a990b500ce04fd5edc57b16ab1
Krish <bkrisanthan@gmail.com>
2016-06-27 20:33:32 -0400

    added new grid functionality

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 $(document).ready(function(){
-	for(var i = 0; i < 16; i++){
-		$("#container").append("<div class='square'></div>");
-	}
+	newGrid(true);
 	$(".square").hover(function(){
 		$(this).css("background-color", "blue");
 	});
 });
+
+var newGrid = function(begin){
+	$("#container").empty();
+	var num = 4;
+	if(!begin){
+		num = prompt("How many squares per row/column do you want?");
+		generateSquares(num);
+		if(num > 4){
+			$(".square").css("width", (158/num));
+			$(".square").css("height", (158/num));
+		} else {
+			$(".square").css("width", (158/num));
+			$(".square").css("height", (158/num));
+		}
+	} else {
+		generateSquares(num);
+	}
+}
+
+var generateSquares = function(num){
+	for(var i = 0; i < Math.pow(num, 2); i++){
+			$("#container").append("<div class='square'></div>");
+		}
+}
